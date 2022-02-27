0dc8d3270dfef5b26adaa4131389c1474024423a
Fran Castillo <castillofranciscodaniel@gmail.com>
2022-02-27 05:04:08 -0300

    fix CountTheLetterCInNameCharacterUseCaseService

--- src/application/use-cases/count-the-letter-c-in-name-character-use-case.service.ts
+++ src/application/use-cases/count-the-letter-c-in-name-character-use-case.service.ts
@@ -2,6 +2,6 @@ import { Injectable } from '@nestjs/common';
 import {CustomHttpService} from "../../infraestructure/clients/custom.http.service";
 
 @Injectable()
-export class CountTheLetterCInNameCharacterUseCaseService extends CustomHttpService<any>{
+export class CountTheLetterCInNameCharacterUseCaseService {
 
 }
