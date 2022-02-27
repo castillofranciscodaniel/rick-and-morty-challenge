54c04cdbc35071886ce1b3ee84c9b6b0cc22e8c5
Fran Castillo <castillofranciscodaniel@gmail.com>
2022-02-27 07:19:15 -0300

    add CountTheLetterCInNameCharacterUseCaseService

---
+++ src/application/dto/count-result.ts
@@ -0,0 +1,6 @@
+export interface CountResult {
+    "char": string,
+    "count": number,
+    "resource": string
+}
+
--- src/application/use-cases/count-the-letter-c-in-name-character-use-case.service.ts
+++ src/application/use-cases/count-the-letter-c-in-name-character-use-case.service.ts
@@ -1,17 +1,47 @@
 import {Injectable} from '@nestjs/common';
 import {CharacterClientService} from "../../infraestructure/clients/character-client.service";
-import {Observable, of} from "rxjs";
+import {catchError, mergeMap, Observable, of, throwError, zip} from "rxjs";
+import {CountResult} from "../dto/count-result";
 
 @Injectable()
 export class CountTheLetterCInNameCharacterUseCaseService {
 
+    letter: 'c'
+    resource: 'resource'
+
     constructor(private readonly characterClient: CharacterClientService) {
     }
 
-    handler(): Observable<any> {
+    handler(): Observable<CountResult> {
 
+        const countResult: CountResult = {
+            count: 0,
+            char: this.letter,
+            resource: this.resource,
+        }
 
-        return this.characterClient.findAll(1);
+        const requests: Observable<any>[] = [];
+        for (let i = 0; i <= 42; i++) {
+            requests.push(this.characterClient.findAll(i))
+        }
+
+        return zip(...requests).pipe(
+            mergeMap(characters => {
+
+                characters.flatMap((character) => {
+                    for (let i = 0; i < character.name?.length; i++) {
+                        if (character.name.toLowerCase().charAt(i) === this.letter) {
+                            countResult.count++
+                        }
+                    }
+                })
+
+                return of(countResult)
+            }),
+            catchError(error => {
+                return throwError(error);
+            })
+        )
     }
 
 }
