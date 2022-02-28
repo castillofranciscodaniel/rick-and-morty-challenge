f562452a29a21230ac25050215bfdbbfde286249
Fran Castillo <castillofranciscodaniel@gmail.com>
2022-02-28 15:27:01 -0300

    refactor variable in constructor

--- src/application/use-cases/counter-exercise-use-case/counter-exercise-use-case.service.ts
+++ src/application/use-cases/counter-exercise-use-case/counter-exercise-use-case.service.ts
@@ -1,21 +1,29 @@
 import {Injectable} from '@nestjs/common';
 import {CharacterClientService} from "../../../infraestructure/clients/character-client.service";
-import {CountTheLetterCInNameCharacterUseCaseService} from "../count-the-letter-c-in-name-character-use-case/count-the-letter-c-in-name-character-use-case.service";
-import {CountTheLetterLInNamesLocationUseCaseService} from "../count-the-letter-l-in-names-location-use-case/count-the-letter-l-in-names-location-use-case.service";
-import {CountTheLetterEInNamesEpisodeUseCaseService} from "../count-the-letter-e-in-names-episode-use-case/count-the-letter-e-in-names-episode-use-case.service";
+import {
+    CountTheLetterCInNameCharacterUseCaseService
+} from "../count-the-letter-c-in-name-character-use-case/count-the-letter-c-in-name-character-use-case.service";
+import {
+    CountTheLetterLInNamesLocationUseCaseService
+} from "../count-the-letter-l-in-names-location-use-case/count-the-letter-l-in-names-location-use-case.service";
+import {
+    CountTheLetterEInNamesEpisodeUseCaseService
+} from "../count-the-letter-e-in-names-episode-use-case/count-the-letter-e-in-names-episode-use-case.service";
 import {CountResult, ExerciseResult} from "../../dto/count-result";
 
 @Injectable()
 export class CounterExerciseUseCaseService {
 
-    private exercise_name = 'Char counter'
-    private maxTimeToExecuteInMilliseconds = 3000
+    private readonly exercise_name;
+    private readonly maxTimeToExecuteInMilliseconds;
 
     constructor(
         private readonly countTheLetterCInNameCharacterUseCaseService: CountTheLetterCInNameCharacterUseCaseService,
         private readonly countTheLetterIInNamesLocationUseCaseService: CountTheLetterLInNamesLocationUseCaseService,
         private readonly countTheLetterEInNamesEpisodeUseCaseService: CountTheLetterEInNamesEpisodeUseCaseService
     ) {
+        this.exercise_name = 'Char counter';
+        this.maxTimeToExecuteInMilliseconds = 3000
     }
 
     async handler(): Promise<ExerciseResult<CountResult>> {
