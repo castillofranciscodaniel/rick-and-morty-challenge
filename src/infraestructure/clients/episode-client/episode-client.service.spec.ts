36ca0df752710fac00147992c715038759e368a0
Fran Castillo <castillofranciscodaniel@gmail.com>
2022-03-01 14:44:14 -0300

    wrrite episode client test

--- src/infraestructure/clients/episode-client/episode-client.service.spec.ts
+++ src/infraestructure/clients/episode-client/episode-client.service.spec.ts
@@ -1,9 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EpisodeClientService } from './episode-client.service';
-import {HttpModule} from "nestjs-http-promise";
+import {HttpModule, HttpService} from "nestjs-http-promise";
+import {AxiosResponse} from "axios";
+import {newEpisodePage1} from "../../../json-to-test";
+import {Pagination} from "../../dto/pagination";
+import {Episode} from "../../../domain/models/episode";
 
 describe('EpisodeClientService', () => {
   let service: EpisodeClientService;
+  let httpService: HttpService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,10 +16,35 @@ describe('EpisodeClientService', () => {
       imports: [HttpModule]
     }).compile();
 
+    httpService = module.get<HttpService>(HttpService);
     service = module.get<EpisodeClientService>(EpisodeClientService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should be return a episodes pagination', async () => {
+
+    const response: AxiosResponse<Pagination<Episode>> = {
+      data: newEpisodePage1(),
+      headers: {},
+      config: {url: 'http://localhost:3000/mockUrl'},
+      status: 200,
+      statusText: 'OK',
+    };
+
+
+    jest.spyOn(httpService, 'get').mockImplementation(async () => {
+      return Promise.resolve(response)
+    });
+
+    expect(await service.findAll(1)).toMatchObject(newEpisodePage1());
+  });
+
+  it('should be throw a exception', async () => {
+
+
+    jest.spyOn(httpService, 'get').mockImplementation(async () => {
+      return Promise.reject("any http error")
+    });
+
+    expect(service.findAll(1)).resolves.toThrowError();
   });
 });
