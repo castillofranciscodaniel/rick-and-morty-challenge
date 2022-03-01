512c7e6d8d8d58f830674054ffced5a92d80ed4a
Fran Castillo <castillofranciscodaniel@gmail.com>
2022-03-01 01:16:16 -0300

    fix CountTheLetterEInNamesEpisodeUseCaseService test with new data in memory

--- src/application/use-cases/count-the-letter-e-in-names-episode-use-case/count-the-letter-e-in-names-episode-use-case.service.spec.ts
+++ src/application/use-cases/count-the-letter-e-in-names-episode-use-case/count-the-letter-e-in-names-episode-use-case.service.spec.ts
@@ -5,19 +5,33 @@ import {ClientsModule} from "../../../infraestructure/clients/clients.module";
 import {EpisodeClientService} from "../../../infraestructure/clients/episode-client.service";
 import {Pagination} from "../../../infraestructure/dto/pagination";
 import {Episode} from "../../../domain/models/episode";
+import {DataInMemoryModule} from "../../../infraestructure/services/data-in-memory/data-in-memory.module";
+import {DataInMemoryService} from "../../../infraestructure/services/data-in-memory/data-in-memory.service";
 
 describe('CountTheLetterEInNamesEpisodeUseCaseService', () => {
     let service: CountTheLetterEInNamesEpisodeUseCaseService;
     let episodeClientService: EpisodeClientService;
+    let dataInMemoryModule: DataInMemoryService;
 
-    beforeEach(async () => {
+    beforeAll(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [CountTheLetterEInNamesEpisodeUseCaseService],
-            imports: [ClientsModule]
+            imports: [ClientsModule, DataInMemoryModule]
         }).compile();
 
         episodeClientService = module.get<EpisodeClientService>(EpisodeClientService);
+        dataInMemoryModule = module.get<DataInMemoryService>(DataInMemoryService);
         service = module.get<CountTheLetterEInNamesEpisodeUseCaseService>(CountTheLetterEInNamesEpisodeUseCaseService);
+
+
+        jest.spyOn(episodeClientService, 'findAll').mockImplementation((page: number) => {
+            if (page === 1) {
+                return Promise.resolve(newEpisodePage1());
+            }
+            return Promise.resolve(newEpisodePage2());
+        });
+
+        await dataInMemoryModule.load();
     });
 
     it('should be defined', () => {
@@ -25,18 +39,12 @@ describe('CountTheLetterEInNamesEpisodeUseCaseService', () => {
     });
 
     it('should be return an CountResult', async () => {
-        jest.spyOn(episodeClientService, 'findAll').mockImplementation((page: number) => {
-            if (page === 1) {
-                return Promise.resolve(newEpisodePage1())
-            }
-            return Promise.resolve(newEpisodePage2())
-        });
 
         const countResult: CountResult = {
             count: 8,
             char: 'e',
             resource: 'episode',
-        }
+        };
 
         expect(await service.handler()).toStrictEqual(countResult);
     });
